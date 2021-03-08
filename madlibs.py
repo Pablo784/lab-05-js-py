c298ad7d449dc68f132e70251b3863023617c37d
Pablo Vasquez <pablo_vasquez@Pablos-MacBook-Pro.local>
2021-03-08 17:54:56 -0500

    These are the madlib files with html and python

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+
+  noun = input("Please type a noun") # this version includes user-submitted values
+ adjective = input("Please type a adjective")
+adverb = input("Please type a adverb")
+verb = input("Please type a verb")
+
+      print("On a tropical  " + str(noun) + ".")
+      print("Underneath a "  + str(adjective) + "lava moon.")
+      print("Hangin' "  + str(adverb) + "with the hulu dancers,")
+      print(str(verb) + "questions cause they got all the answers.")
